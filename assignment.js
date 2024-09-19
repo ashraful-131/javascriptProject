f5b02f11a280110310d5ccd16a7ceb17efb9ec88
Ashraful Islam <ashraful4486@gmail.com>
2024-09-19 21:50:10 +0600

    Add files via upload
    This is javascript project.

---
+++ assignment.js
@@ -0,0 +1,80 @@
+// Quedtion 1 //
+
+let carName = "Volvo";
+
+console.log(carName)
+
+// Quedtion 2 //
+
+let firstName = "Ashraful";
+let lastName = "islam";
+let age = 20;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+// Quedtion 3 //
+
+let x = 10;
+let y = 5;
+
+let z = x * y
+
+console.log(z)
+
+// Quedtion 4 //
+
+let length = 16; // This is length/distance 
+let fulltName_ = "piyash"; // This is fullname
+
+const A = {
+    f_name: "piyash", // This is firstname
+    l_name: "khandaker" //This is lastname
+}
+
+console.log() // This is run funtion
+
+// Quedtion 5 check file - 5. alert msg.html //
+
+// Quedtion 6 check file - 6. alert msg2.html //
+
+// Quedtion 7 check file - 7. button.html //
+
+// Quedtion 8 //
+
+let cars = ['Volvo', 'Jeep', 'Mercedes'];
+cars[0] = 'Ford';
+
+console.log(cars)
+
+// Quedtion 9 //
+
+// Q-1 //
+let Random = Math.random();
+console.log(Random)
+
+// Q-2 //
+let largest = Math.max(10,12,14,16,18,20);
+console.log(largest)
+
+// Q-3 //
+let square = Math.sqrt(9);
+console.log(square)
+
+// Quedtion 10 //
+
+// Q-1 //
+let m = 10
+let n = 5;
+
+let result = m > n;
+
+console.log(result)
+
+
+// Q-2 //
+
+ let Age = 18;
+ let massage = age > "Too young" ? 18 : "Old enough";
+ console.log(massage)
